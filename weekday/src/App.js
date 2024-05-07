3d51d8b2c481c7bef09296a89d8d44ef3baa98c6
chiragmathur27 <pratiksh.chirag@gmail.com>
2024-05-07 22:19:23 +0530

    infinite scroll

--- weekday/src/App.js
+++ weekday/src/App.js
@@ -1,33 +1,52 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
 import "./App.css";
 import CompanyCard from "./Components/Card";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import {
-  Avatar,
-  AvatarGroup,
-  Box,
-  Button,
-  CardActionArea,
-  Chip,
-  Grid,
-  Paper,
-} from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [offset, setOffset] = useState(2);
+  const [loading, setLoading] = useState(true);
+  const fetchData = async () => {
+    const body = {
+      limit: 12,
+      offset: offset,
+    };
+    const res = await axios.post(
+      "https://api.weekday.technology/adhoc/getSampleJdJSON",
+      body
+    );
+
+    setData((prev = []) => [...prev, ...res.data["jdList"]]);
+    setLoading(false);
+  };
+
+  const handleScroll = async () => {
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setOffset((prev) => prev + 1);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [offset]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    // return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <div>
-        <h3 className="name">Company Name</h3>
-        <h2 className="App">Frontend Engineer</h2>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa
-          consequatur porro id cupiditate exercitationem praesentium ut, ad,
-          nesciunt voluptas asperiores. Modi inventore consequuntur eum!
-          Accusantium temporibus laudantium ducimus asperiores!
-        </div>
-      </div>
       <Box sx={{ width: "100%", height: "100%" }}>
         <Grid container spacing-xs-3 className="grid_main">
           <Grid item xs={12} className="grid_item1">
@@ -37,9 +56,9 @@ function App() {
               className="grid_main"
               style={{ marginLeft: "auto", marginRight: "auto" }}
             >
-              <CompanyCard />
-              <CompanyCard />
-              <CompanyCard />
+              {data?.map(() => (
+                <CompanyCard />
+              ))}
             </Grid>
           </Grid>
         </Grid>
